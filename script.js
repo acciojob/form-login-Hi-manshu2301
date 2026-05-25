67187082e07ec56b3fbad4435f4f8efc82a99945
Hi-manshu2301 <104667314+Hi-manshu2301@users.noreply.github.com>
2026-05-25 17:50:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function getFormvalue() {
     //Write your code here
-	let first = document.getElementById("fname").value;
-	let last = document.getElementById("lname").value;
-	alert(first+" "+last);
+	event.preventDefault();
+	let first = document.getElementsByName("fname")[0].value;
+	let last = document.getElementsByName("lname")[0].value;
+	alert(first + " " + last);
 
 }
 const submitBtn = document.querySelector('input[type="submit"]');
